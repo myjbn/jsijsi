ee0aab5425ab175de1712672b07722c508b1bdab
My ZB <hdt.umm@gmail.com>
2024-03-21 19:41:43 +0700

    websiteforjsi

---
+++ themify-icons-font/themify-icons/DesProgram.java
@@ -0,0 +1,70 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
+public class DesProgram { // creating an instance of the Cipher class for encryption
+    private static Cipher encrypt; // creating an instance of the Cipher class for decryption
+    private static Cipher decrypt; // initializing vector
+    private static final byte[] initialization_vector = { 22, 33, 11, 44, 55, 99, 66, 77 }; // main() method
+
+    public static void main(String[] args) { // path of the file that we want to
+        String textFile = "B:/Desktop/DemoData.txt"; // path of
+        String encryptedData = "B:/Desktop/encrypteddata.txt"; // path of the decrypted file
+        String decryptedData = "B:/Desktop/decrypteddata.txt";
+        try { // generating keys by
+            SecretKey scrtkey = KeyGenerator.getInstance("DES").generateKey();
+            AlgorithmParameterSpec aps = new IvParameterSpec(initialization_vector); // setting encryption mode
+            encrypt = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            encrypt.init(Cipher.ENCRYPT_MODE, scrtkey, aps); // setting decryption mode
+            decrypt = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            decrypt.init(Cipher.DECRYPT_MODE, scrtkey, aps); // calling encrypt() method
+            encryption(new FileInputStream(textFile), new FileOutputStream(encryptedData)); // calling decrypt() method
+                                                                                            // to decrypt the
+            decryption(new FileInputStream(encryptedData), new FileOutputStream(decryptedData)); // prints the stetment
+                                                                                                 // if the program runs
+            System.out.println("The encrypted and decrypted files have been created successfully.");
+        } // catching multiple exceptions by using the | (or)
+        catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException
+                | InvalidAlgorithmParameterException | IOException e) { // prints the message (if any) related to
+                                                                        // exceptions
+            e.printStackTrace();
+        }
+    } // method for encryption
+
+    private static void encryption(InputStream input, OutputStream output)
+            throws IOException {
+        output = new CipherOutputStream(output, encrypt); // calling the
+        writeBytes(input, output);
+    }
+
+    // method for decryption
+    private static void decryption(InputStream input, OutputStream output)
+            throws IOException {
+        input = new CipherInputStream(input, decrypt); // calling the writeBytes() method to write
+        writeBytes(input, output);
+    } // method for writting bytes to the files
+
+    private static void writeBytes(InputStream input, OutputStream output) throws IOException {
+        byte[] writeBuffer = new byte[512];
+        int readBytes = 0;
+        while ((readBytes = input.read(writeBuffer)) >= 0) {
+            output.write(writeBuffer, 0, readBytes);
+        }
+        // closing the output stream
+        output.close(); // closing the input stream
+        input.close();
+    }
+}
